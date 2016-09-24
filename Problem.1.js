1fa9db8e76189648ebe2d3b17bc1b333ab8556dd
davidkeirn2018 <davidkeirn2018@u.northwestern.edu>
2016-09-24 16:18:28 -0500

    Add files via upload

--- Problem.1.js
+++ Problem.1.js
@@ -13,4 +13,33 @@ for (var x=1; x <= 100; x++)
         console.log("Buzz");
     else
         console.log(x);
+
+
+    var fs = require("fs");
+    var readMe = fs.readFileSync('Assignment1_names-1.txt', 'utf8');
+    console.log(readMe);
+
+
+    /* Everything below here works.. kinda'
+fs.readFile('Assignment1_names-1.txt', function(err, data){
+    var names = data.toString().split('\n');
+    names.sort(function(a, b){return a-b});
+    console.log(data.toString());*/
+};
+
+
+
+
+/*var array = fs.readFileSync('Assignment1_names-1.txt').toString().split("\n");
+for(i in array) {
+   console.log(array[i]);
 }
+
+console.log('Executed Before File Reading');
+
+fs.readFile('./test.project.01/Assignment1_names-1.txt', 'utf8', function(error, data) {
+    console.log(data);
+});
+
+console.log('Executed After File Reading.. maybe');
+*/
